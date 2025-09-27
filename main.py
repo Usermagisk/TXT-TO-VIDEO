feddec22c3943a65aa7f985efdff0d255cd1c3bc
Usermagisk <146827693+Usermagisk@users.noreply.github.com>
2025-09-27 17:15:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,8 +47,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-return web.json_response("your_render_url") ## change it with your host url
-
+    return web.json_response("your_render_url")  ## change it with your host url
 async def web_server():
 web_app = web.Application(client_max_size=30000000)
 web_app.add_routes(routes)
